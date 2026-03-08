67c6556c47c63e393e69947edf3e605e7877e06d
sthanfv <fv9316@gmail.com>
2026-03-08 06:09:24 -0500

    chore(release): consolidación v1.9.2 - Carga condicional de Pixel y motor Edge SHA-256 finalizado 🚀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ const outfit = Outfit({
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 const GSC_ID = process.env.NEXT_PUBLIC_GSC_ID;
+const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
 export const viewport: Viewport = {
   themeColor: '#ffbf00',
@@ -194,24 +195,27 @@ export default function RootLayout({
           </FirebaseClientProvider>
         </ThemeProvider>
         <Analytics />
-        {/* INYECTA EL PIXEL DE META DE FORMA SEGURA */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('track', 'PageView');
-          `,
-          }}
-        />
+        {/* INYECTA EL PIXEL DE META DE FORMA SEGURA SI EXISTE EL ID */}
+        {META_PIXEL_ID && (
+          <Script
+            id="meta-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+        )}
         <PwaRegistry />
       </body>
     </html>
