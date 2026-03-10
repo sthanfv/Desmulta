0f9443f718fd76cb21e4ca4fbf12600551d464ef
sthanfv <fv9316@gmail.com>
2026-03-09 20:54:46 -0500

    fix: corregir advertencia de input controlado en ConsultationForm y limpieza de cache v2.3.1

--- src/components/vial-clear/ConsultationForm.tsx
+++ src/components/vial-clear/ConsultationForm.tsx
@@ -109,6 +109,7 @@ export function ConsultationForm({ onSuccess, mode = 'full' }: ConsultationFormP
           tipoInfraccion: '',
           estadoCoactivo: '',
           evidenceUrl: '',
+          email: '',
         },
   });
 
