0b474d9cfd4343b2c8c73e6554c47743a093ab22
sthanfv <fv9316@gmail.com>
2026-07-16 19:47:24 -0500

    correcion: agregar ID de chat privado de Sthan al allowlist del webhook en producción

--- functions/src/telegramWebhook.ts
+++ functions/src/telegramWebhook.ts
@@ -314,6 +314,7 @@ export const telegramWebhook = onRequest(
       ...parseAllowedIds(process.env.TELEGRAM_CHAT_ID),
       ...parseAllowedIds(process.env.TELEGRAM_DEV_CHAT_ID),
       ...parseAllowedIds(process.env.TELEGRAM_SECURITY_CHAT_ID),
+      6343332379, // ID del Administrador Maestro (Sthan)
     ];
 
     const incomingChatId =
